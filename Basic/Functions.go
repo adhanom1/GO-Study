ed597adf9e8942ac11805975219103103c03f124
adhanom1 <adhanomkidanmariamg@gmail.com>
2023-03-23 18:14:23 +0300

    init function in use

---
+++ Basic/Functions.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+const a string ="hello am testing init fuction and compare it"
+func init(){
+	fmt.Println(a)
+
+}
+func main(){
+	fmt.Println("hello am from main fuction ")
+}
