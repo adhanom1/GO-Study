025a7f04aa07a8fb7731ce578da8a1086a4dcfe8
adhanom1 <adhanomkidanmariamg@gmail.com>
2023-03-24 12:32:44 +0300

    casting values

--- Basic/casting.go
+++ Basic/casting.go
@@ -1,9 +1,20 @@
 package main
 import "fmt"
- 
+ type (
+	i int 
+	f float32
+	s string 
+ )
  func main(){
 	a:=10.55
 	b:=int(a)
+	f:=4141.32
+	i:=int(f)
+	s:="hello there"
 	fmt.Println("the value is float:",a)
 	fmt.Println("the value is casted in to intger:",b)
+	fmt.Println("value of type f is float before casting ",f)
+	fmt.Println("value of the type i is interger ",i)
+	fmt.Println(s)
+
  }
