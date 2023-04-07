6ad28d3e57a4e463ee91be488ad2f4982e57fce0
adhanom1 <adhanomkidanmariamg@gmail.com>
2023-04-07 12:14:50 +0300

    Basic Concept

--- Basic/String_package.go
+++ Basic/String_package.go
@@ -5,10 +5,20 @@ import (
 	"strings"
 )
 
+/*
+	package strings has many function on this example
+
+we will se three function that are basic like
+
+	         hasprefix function with two input ( the string ,specific given value string we have been given)
+			 hassuffix function with two input (the string , specific given value to compare with the string we have on hand )
+			 contains  a function to search for specific character or string on given string
+*/
 func main() {
 	st := "hello world how are you ?"
 	fmt.Println("is the string %s has prefix of %s", st)
 	fmt.Println(strings.HasPrefix(st, "he"))
 	fmt.Println(strings.HasPrefix(st, "world"))
 	fmt.Println(strings.HasSuffix(st, "?"))
+	fmt.Println(strings.Contains(st, "how"))
 }
