9ad15cfe60cc0e617d7cf7cb6c22427000348cb9
adhanom1 <adhanomkidanmariamg@gmail.com>
2023-04-07 12:51:58 +0300

    Basic Concept

--- Basic/String_package.go
+++ Basic/String_package.go
@@ -10,18 +10,28 @@ import (
 
 we will se three function that are basic like
 
-		         hasprefix function with two input ( the string ,specific given value string we have been given)
-				 hassuffix function with two input (the string , specific given value to compare with the string we have on hand )
-				 contains  a function to search for specific character or substring on given string
-	             index function return the sub string starting point
+					         Hasprefix function with two input ( the string ,specific given value string we have been given)
+							 Hassuffix function with two input (the string , specific given value to compare with the string we have on hand )
+							 Contains  a function to search for specific character or substring on given string
+				             Index function return the sub string starting point
+							 Count function for counting the occurences of the substring or character in string
+							 Repeat function repeat the string given with given intger value return string
+		                     ToLower to change the string in to small letter
+							 ToUpper to chage the string in to all capital letter
+	                         Trim to remove substrings from string
 */
 func main() {
-	st := "hello world how are you ?"
-	fmt.Println("is the string %s has prefix of %s", st)
-	fmt.Println(strings.HasPrefix(st, "he"))
+	st := "Hello World How are you ?"
+	fmt.Printf("is the string %s has prefix of %s", st, strings.HasPrefix(st, "He"))
+	fmt.Println(strings.HasPrefix(st, "He"))
 	fmt.Println(strings.HasPrefix(st, "world"))
 	fmt.Println(strings.HasSuffix(st, "?"))
 	fmt.Println(strings.Contains(st, "how"))
-	fmt.Println(strings.Index(st, "how"))
+	fmt.Println(strings.Index(st, "How"))
 	fmt.Println(strings.LastIndex(st, "how"))
+	fmt.Println(strings.Count(st, "l"))
+	fmt.Println(strings.Repeat(st, 2))
+	fmt.Println(strings.ToLower(st))
+	fmt.Println(strings.ToUpper(st))
+	fmt.Println(strings.Trim(st, "Hello World"))
 }
