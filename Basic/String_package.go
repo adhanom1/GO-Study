9c6d80905ee2d6a4d350c6c96949cea45f0f32b2
adhanom1 <adhanomkidanmariamg@gmail.com>
2023-04-07 13:21:04 +0300

    Basic Concept

--- Basic/String_package.go
+++ Basic/String_package.go
@@ -10,18 +10,20 @@ import (
 
 we will se three function that are basic like
 
-					         Hasprefix function with two input ( the string ,specific given value string we have been given)
-							 Hassuffix function with two input (the string , specific given value to compare with the string we have on hand )
-							 Contains  a function to search for specific character or substring on given string
-				             Index function return the sub string starting point
-							 Count function for counting the occurences of the substring or character in string
-							 Repeat function repeat the string given with given intger value return string
-		                     ToLower to change the string in to small letter
-							 ToUpper to chage the string in to all capital letter
-	                         Trim to remove substrings from string
+						         Hasprefix function with two input ( the string ,specific given value string we have been given)
+								 Hassuffix function with two input (the string , specific given value to compare with the string we have on hand )
+								 Contains  a function to search for specific character or substring on given string
+					             Index function return the sub string starting point
+								 Count function for counting the occurences of the substring or character in string
+								 Repeat function repeat the string given with given intger value return string
+			                     ToLower to change the string in to small letter
+								 ToUpper to chage the string in to all capital letter
+		                         Trim to remove substrings from string
+	                             Field function
+								 Split  function
 */
 func main() {
-	st := "Hello World How are you ?"
+	st := "Hello World | How are you ?"
 	fmt.Printf("is the string %s has prefix of %s", st, strings.HasPrefix(st, "He"))
 	fmt.Println(strings.HasPrefix(st, "He"))
 	fmt.Println(strings.HasPrefix(st, "world"))
@@ -33,5 +35,18 @@ func main() {
 	fmt.Println(strings.Repeat(st, 2))
 	fmt.Println(strings.ToLower(st))
 	fmt.Println(strings.ToUpper(st))
-	fmt.Println(strings.Trim(st, "Hello World"))
+	fmt.Println(strings.Trim(st, "\r\n"))
+	str := strings.Fields(st)
+	for _, val := range str {
+		fmt.Printf("%s -", val)
+	}
+	fmt.Println()
+	str2 := strings.Split(st, "|")
+	fmt.Println(str2)
+	for _, val2 := range str2 {
+		fmt.Printf("%s -", val2)
+	}
+	fmt.Println()
+	str3 := strings.Join(str2, "james bond")
+	fmt.Printf("sl2 joined by ;: %s\n", str3)
 }
