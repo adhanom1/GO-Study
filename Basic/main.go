1bd6963ce4396d6fec26b0a504203275e7783d03
adhanom1 <adhanomkidanmariamg@gmail.com>
2023-09-17 14:07:27 +0300

    local function import

---
+++ Basic/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"news.com/event/Casting"
+)
+
+func main() {
+	var A string = "adhanom"
+	n, err := fmt.Println("hello world")
+	fmt.Println(n)
+	fmt.Println(err)
+	fmt.Printf("the name is %v", A)
+	fmt.Printf("%#v", A)
+	Casting.Adhanom()
+}
