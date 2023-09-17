1abdc3cf3fe0732e678851f16e0bd8be720a753f
adhanom1 <adhanomkidanmariamg@gmail.com>
2023-09-17 20:17:51 +0300

    Print function implementation

--- Basic/main.go
+++ Basic/main.go
@@ -6,6 +6,8 @@ import (
 	"news.com/event/Casting"
 )
 
+type hotdog int
+
 func main() {
 	var A string = "adhanom"
 	n, err := fmt.Println("hello world")
@@ -14,4 +16,7 @@ func main() {
 	fmt.Printf("the name is %v", A)
 	fmt.Printf("%#v", A)
 	Casting.Adhanom()
+	var ad hotdog = 50
+	fmt.Println(ad)
+
 }
