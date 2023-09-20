101253e31e858bc165c0a24e3df087370583ed3c
adhanom1 <adhanomkidanmariamg@gmail.com>
2023-09-20 20:48:56 +0300

    numeric  value initiation and usage

--- Basic/numeric_type.go
+++ Basic/numeric_type.go
@@ -3,10 +3,20 @@ package main
 import "fmt"
 
 func main() {
+	/* the numerical type are integer , floating and complex value .
+	on the contect of integer there are two different catagories
+	# unsigned integer
+	# signed integer
+	the difference between then this the use of values contaning negeative values or haveing used all
+	the values for positive numbers like uint8 is from -128 to 128 but if we used int8 is all value
+	from 0 to 256
+	*/
 	x := 42
 	y := 45.66
+	var w int8 = 127
 	fmt.Println(x)
 	fmt.Println(y)
+	fmt.Println(w)
 	fmt.Printf("%T\n", x)
 	fmt.Printf("%T\n", y)
 }
