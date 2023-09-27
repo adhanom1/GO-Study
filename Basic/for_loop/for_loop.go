93ed8ad946fb41ce97a42d412755ab4d1408d0a2
adhanom1 <adhanomkidanmariamg@gmail.com>
2023-09-28 00:49:17 +0300

    for loop implementation

--- Basic/for_loop/for_loop.go
+++ Basic/for_loop/for_loop.go
@@ -3,13 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	sum := 0
+	sum := 1
 	for i := 0; i < 10; i++ {
 		sum = sum + 1
 
 	}
 	fmt.Println(sum)
 	// with out puting init and post statement those two are optional
+	for sum < 10 {
+		sum += sum
+	}
+	fmt.Println(sum)
+	/// for loop with init and post statment will act like while loop in c and c like language
 	for sum < 10 {
 		sum += sum
 	}
