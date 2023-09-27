ad5fb45d9b3eace6b173056a36456c79d2080029
adhanom1 <adhanomkidanmariamg@gmail.com>
2023-09-27 22:02:24 +0300

    basic in  go lang

--- Basic/import _in_go/import.go
+++ Basic/import _in_go/import.go
@@ -7,6 +7,8 @@ import (
 
 func main() {
 	fmt.Println("the number that has been generated was ", rand.Float32())
+	//fmt.Println(rand.float32())     //is error because with rand package if method or function is has to be exported it should be capital latter the first latter
+	// this apply in identifer in any case like funtions , variables , slice
 	fmt.Println("the number that been generated was ", rand.Intn(50))
 
 }
