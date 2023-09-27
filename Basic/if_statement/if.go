e2dbc1b91c98dd20770c3619159853af704e150d
adhanom1 <adhanomkidanmariamg@gmail.com>
2023-09-28 01:10:32 +0300

    if  implementation

--- Basic/if_statement/if.go
+++ Basic/if_statement/if.go
@@ -11,7 +11,14 @@ func sqrt(x float64) string {
 	}
 	return fmt.Sprint(math.Sqrt(x))
 }
-
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	}
+	return lim
+}
 func main() {
 	fmt.Println(sqrt(2), sqrt(-4))
+	fmt.Println(pow(2, 3, 10))
+	fmt.Println(pow(3, 3, 25))
 }
