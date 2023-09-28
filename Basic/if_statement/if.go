66bc4128e90d812f1d064d447e4b9f2ce9b250ea
adhanom1 <adhanomkidanmariamg@gmail.com>
2023-09-28 13:22:34 +0300

    basic implematation

--- Basic/if_statement/if.go
+++ Basic/if_statement/if.go
@@ -17,8 +17,18 @@ func pow(x, n, lim float64) float64 {
 	}
 	return lim
 }
+func pow2(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	// can't use v here, though
+	return lim
+}
 func main() {
 	fmt.Println(sqrt(2), sqrt(-4))
 	fmt.Println(pow(2, 3, 10))
 	fmt.Println(pow(3, 3, 25))
+	fmt.Println(pow2(2, 3, 10))
 }
