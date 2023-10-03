b08c11cf7d110979cb91b5953d36991788c5b459
adhanom1 <adhanomkidanmariamg@gmail.com>
2023-10-03 03:21:10 +0300

    switch statement

--- Basic/switch_statement/switch.go
+++ Basic/switch_statement/switch.go
@@ -3,9 +3,26 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"time"
 )
 
 func main() {
+	today := time.Now().Weekday()
+	//fmt.Println(time.Now())
+	fmt.Println(today)
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("today is saturdat")
+	case today + 1:
+		fmt.Println("saturday is tommorrow")
+	case today + 2:
+		fmt.Println("saturday is the day after 2  days ")
+	case today + 3:
+		fmt.Println("saturday is the day after 3 days ")
+	default:
+		fmt.Println("saturday is far way ")
+	}
+
 	switch os := runtime.GOOS; os {
 	case "darwin":
 		fmt.Println("OS,x")
